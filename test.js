8f87c1c74cb8197b2830dbf26f6fdfbecc75fa7d
2youyouo2 <501251991@qq.com>
2019-01-22 18:16:35 +0800

    add git-deploy

--- test.js
+++
@@ -1,8 +0,0 @@
-const deploy = require('./deploy');
-const path = require('path');
-
-deploy({
-    base_dir: __dirname,
-    deploy_dir: './build/web-mobile',
-    repo: 'git@github.com:2youyou2/TheAviator.git'
-});
