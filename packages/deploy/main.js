45bc0b6ac9892113303c4160679f4bfb69f870bc
2youyouo2 <501251991@qq.com>
2019-01-23 17:27:55 +0800

    add deploy menu

---
+++ packages/deploy/main.js
@@ -0,0 +1,36 @@
+
+const spawn = require('child_process').spawn;
+
+let child;
+
+function log (data) {
+    let infos = data.toString().split('\n');
+    infos.forEach(info => Editor.log(info));
+}
+
+module.exports = {
+    messages: {
+        'deploy-the-aviator' (event) {
+
+            if (child) {
+                Editor.warn('Already running deploy-the-aviator');
+                return;
+            }
+
+            Editor.info('Start to deploy the aviator.');
+
+            child = spawn('npm', ['run', 'deploy'], {
+                cwd: Editor.Project.path
+            });
+            child.stdout.on('data', function (data) {
+                log(data);
+            });
+            child.stderr.on('data', function (data) {
+                log(data);
+            });
+            child.on('close', function () {
+                child = null;
+            });
+        }
+    }
+};
